889c1f4ade9b0017fac0d467ec2ec578e5f1a306
Chris Bornholdt <chris.bornholdt@gmail.com>
2018-06-09 12:18:24 -0400

    Blue blood/leaking for androids

---
+++ Source/Androids/Extensions/PawnExt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+//Extension method, in RimWorld namespace
+namespace RimWorld
+{
+    //Make sure I bind to the Android fleshDef as early as possible AFTER they are loaded
+    [StaticConstructorOnStartup]
+    static public class PawnExt
+    {
+		static readonly public FleshTypeDef androidFlesh;
+
+		static PawnExt()
+		{
+			androidFlesh = DefDatabase<FleshTypeDef>.GetNamed("Android");
+		}
+    
+        static public bool IsAndroid(this Pawn pawn)
+        {
+			return pawn.RaceProps.FleshType == androidFlesh;
+        }
+    }
+}
---
+++ Source/Androids/Patches/HarmonyPatches.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Harmony;
+using Verse;
+using RimWorld;
+
+namespace BlueLeakTest
+{
+    [StaticConstructorOnStartup]
+    static public class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.rwmods.androidtiers");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
